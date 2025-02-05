e3ec31378e29fb08f4e14f3077985cf641d9ce36
Fernando Almeida <fernando@macpg.com>
2025-02-05 12:26:08 -0300

    initial commit

---
+++ scenes/character_body_2d.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var _velocity = Vector2.ZERO
+var _sprites = {Vector2.RIGHT: 1, Vector2.LEFT: 2, Vector2.UP: 3, Vector2.DOWN: 4}
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Once again, we call `Input.get_action_strength()` to support analog movement.
+	var x : int = 0
+	var y : int = 0
+	
+	if Input.is_action_pressed('ui_left'):
+		x -= 1
+	if Input.is_action_pressed('ui_right'):
+		x += 1
+	if Input.is_action_pressed('ui_up'):
+		y -= 1
+	if Input.is_action_pressed('ui_down'):
+		y += 1
+	
+	var direction := Vector2(x, y)
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+		
+	if direction.length() == 0:
+		animated_sprite.play('idle')
+	else:
+		animated_sprite.play('walking')
+		animated_sprite.flip_h = direction.x < 0
+	
+	self.position += SPEED * delta * direction
+	move_and_slide()
+	
