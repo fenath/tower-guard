c5dbbfe865b377a96e4e5c58b44a60cab39a8743
Fernando Almeida <fernando@macpg.com>
2025-02-07 14:45:15 -0300

    adiciona audio para pickup

---
+++ scripts/gold_entity.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation.play("spawn")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	animation.play("idle")
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,6 +3,24 @@ class_name Hud extends Control
 
 @onready var current_hp: int = 0
 
+@onready var meat_qty: int = 0: set =  _set_meat
+@onready var gold_qty: int = 0: set =  _set_gold
+@onready var wood_qty: int = 0: set =  _set_wood
+
+func _set_meat(value):
+	meat_qty = value
+	$meat/Label.text = label_txt(value)
+
+func _set_gold(value):
+	gold_qty = value
+	$gold/Label.text = label_txt(value)
+
+func _set_wood(value):
+	wood_qty = value
+	$wood/Label.text = label_txt(value)
+
+func label_txt(value: int) -> String:
+	return str(value) + 'x'
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,3 +43,8 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	update_current_hp(current_hp - 1)
+
+
+func _on_button_3_pressed() -> void:
+	$pickupSound.play()
+	meat_qty += 1
