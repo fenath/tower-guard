865fa0be2139b167ee18932c4ce75fcb38444267
Fernando Almeida <fernando@macpg.com>
2025-02-12 15:40:30 -0300

    change hit signal to use attack

--- scripts/hitbox_component.gd
+++ scripts/hitbox_component.gd
@@ -2,9 +2,9 @@ class_name HitboxComponent extends Area2D
 
 @export var health_component: HealthComponent
 
-signal hit
+signal hit(attack: Attack)
 
 func damage(attack: Attack) -> void:
 	if health_component:
 		health_component.damage(attack)
-	hit.emit()
+	hit.emit(attack)
