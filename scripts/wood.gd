69525966e4fd28814a1fc663a4cf56372d02617b
Fernando Almeida <fernando@macpg.com>
2025-02-20 11:39:44 -0300

    remove funcao drop da wood para usar o nó Drop

--- scripts/wood.gd
+++ scripts/wood.gd
@@ -2,9 +2,6 @@ class_name Wood extends Area2D
 
 signal wood_up #sinal da colisão do wood com player
 
-var state: Node2D = null
-
-@onready var dropping_state = $Dropping
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 #Verificação da colisão do wood com player
@@ -12,20 +9,8 @@ func _on_body_entered(_body):
 	hide()
 	wood_up.emit()
 	queue_free()
-	
-func drop_to(target: Vector2) -> void:
-	self.remove_from_group('resources')
-	state = dropping_state
-	dropping_state.node = self
-	dropping_state.origin = self.global_position
-	dropping_state.target = target
-	dropping_state.dropped.connect(_on_dropped)
+
+func spawn() -> void:
 	sprite.play('spawn')
 	await sprite.animation_finished
 	sprite.play('idle')
-	self.add_to_group('resources')
-	
-	
-func _on_dropped() -> void:
-	state.queue_free()
-	state = null
